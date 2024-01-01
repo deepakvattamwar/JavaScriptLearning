1444d6e99af1a7abc78a6c7418c55c941b48ce94
deepakvattamwar <123025015+deepakvattamwar@users.noreply.github.com>
2024-01-01 14:20:27 +0000

    today work done

---
+++ _01_basic/02_Datatypes.js
@@ -0,0 +1,12 @@
+"use strict"// treat all js code as newer version
+ 
+//number 2 power 52 is range
+//String =>""
+//boolean => true/false
+//null => standalone value
+//undefined 
+//symbol => unique
+
+//typeof is used to which typeof datatype 
+
+console.log(typeof null);
