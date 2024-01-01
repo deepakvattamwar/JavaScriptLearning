748abb5b5463f6442be8470f518ad3c06f2af954
deepakvattamwar <123025015+deepakvattamwar@users.noreply.github.com>
2024-01-01 15:03:23 +0000

    conversion Basic done

--- _01_basic/02_Datatypes.js
+++ _01_basic/02_Datatypes.js
@@ -8,5 +8,35 @@
 //symbol => unique
 
 //typeof is used to which typeof datatype 
+//datatype conversion
 
-console.log(typeof null);
+let score=null;
+
+let StringToNumber=Number(score)
+
+console.log(typeof StringToNumber)
+console.log(StringToNumber);
+
+//convertion any dataytype to number
+// "22" => got 22  and type is number 
+//"222abc" => got NaN  and type is number
+// true => got 1 and type is number || false => got 0 
+
+//convertion any type to boolean
+//"" => got false  and type is boolean
+//"deepak" => got true 
+//1 => got true
+
+let a;
+let convertToNumber=Boolean(a)
+console.log(convertToNumber);
+console.log(typeof convertToNumber);
+
+//conversion any type to String
+let b=1234;
+
+let c=String(b);
+
+console.log(c)
+
+console.log(typeof c);
